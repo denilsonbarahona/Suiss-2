09da6d79a761ae97dcdf6a8d47f1f5bcaa714b1b
Denilson Barahona <denilsonbarahona@Denilsons-MacBook-Air.local>
2024-08-26 19:29:47 -0600

    cambios dev cambio datos

--- app/(default)/dashboard/page.tsx
+++ app/(default)/dashboard/page.tsx
@@ -1,41 +1,41 @@
-'use client';
+"use client";
 
-import WelcomeBanner from './welcome-banner';
-import SuissBarChart from '@/components/SuissCharts/suissBarChart';
-import SuissCakeChart from '@/components/SuissCharts/SuissCakeChart';
-import DashboardReports from '@/components/cards/dashboardReports';
-import ExecutionReportIcon from '@/components/icons/ExecutionReport';
-import PerformanceReportIcon from '@/components/icons/PerformanceReport';
-import BeneficiariesReportIcon from '@/components/icons/BeneficiariesReport';
+import WelcomeBanner from "./welcome-banner";
+import SuissBarChart from "@/components/SuissCharts/suissBarChart";
+import SuissCakeChart from "@/components/SuissCharts/SuissCakeChart";
+import DashboardReports from "@/components/cards/dashboardReports";
+import ExecutionReportIcon from "@/components/icons/ExecutionReport";
+import PerformanceReportIcon from "@/components/icons/PerformanceReport";
+import BeneficiariesReportIcon from "@/components/icons/BeneficiariesReport";
 
 const dashboardReports = [
   {
-    title: 'Informe de Ejecución',
+    title: "Informe de Ejecución",
     description:
-      'Datos sobre la ejecución de los programas, incluyendo porcentajes de avance y cumplimiento de metas.',
+      "Datos sobre la ejecución de los programas, incluyendo porcentajes de avance y cumplimiento de metas.",
     icon: ExecutionReportIcon,
   },
   {
-    title: 'Informe de Desempeño',
+    title: "Informe de Desempeño",
     description:
-      'Evaluaciones de impacto y resultados obtenidos por los programas.',
+      "Evaluaciones de impacto y resultados obtenidos por los programas.",
     icon: PerformanceReportIcon,
   },
   {
-    title: 'Informe de Beneficiarios',
+    title: "Informe de Beneficiarios",
     description:
-      'Información sobre la cantidad de personas beneficiadas y sus características.',
+      "Información sobre la cantidad de personas beneficiadas y sus características.",
     icon: BeneficiariesReportIcon,
   },
 ];
 
 export default function Dashboard() {
   return (
-    <div className='px-4 sm:px-6 lg:px-8 py-8 w-full max-w-[96rem] mx-auto'>
+    <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-[96rem] mx-auto">
       <WelcomeBanner />
 
       {/* Cards */}
-      <div className='grid grid-cols-12 gap-6'>
+      <div className="grid grid-cols-12 gap-6">
         <SuissBarChart />
         <SuissCakeChart />
         {dashboardReports.map((report, index) => (
